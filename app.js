8f5bba8e97e6267dcbf994974c363561d29af67a
hasansami75 <mehedihasan3175@gmail.com>
2021-02-07 23:40:56 +0600

    modify card

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const foodName = inputFood => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputFood}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => latestInterface(data))
+        .then(res => res.json())
+        .then(data => latestInterface(data))
 }
 
 const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('click', () =>{
+searchButton.addEventListener('click', () => {
     const food = document.getElementById('food').value;
     foodName(food);
 })
-
-const latestInterface = meal =>{
+// function for showing meal list
+const latestInterface = meal => {
     let find = meal.meals;
     const foodList = document.getElementById('show-food');
     find.forEach(find => {
         const foodDiv = document.createElement('div');
         foodDiv.className = 'div-arrange';
         foodDiv.id = 'foodClick'
-        const divInfo =`
+        const divInfo = `
         <img src= "${find.strMealThumb}">
         <h6>"${find.strMeal}"</h6>
         <button class="btn btn-outline-dark" onclick="foodDetails(${find.idMeal})"> Details </button>
@@ -28,35 +28,41 @@ const latestInterface = meal =>{
     });
 }
 
-    const foodDetails = idMeal => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+//function for showing food details
+const foodDetails = idMeal => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
         .then(res => res.json())
         .then(data => {
             singleFoodDetails(data.meals[0])
         });
-    }
+}
 
-const singleFoodDetails = food =>{
+const singleFoodDetails = food => {
     console.log(food);
     const foodItems = document.getElementById('foodies');
-    
     const ul = document.createElement('ul');
-        const foodIngredients = `
-        <img src= "${food.strMealThumb}">
-        <h4>${food.strMeal}</h4>
-        <h6>Ingredients</h6>
-        <li>${food.strIngredient1}</li>
-        <li>${food.strIngredient2}</li>
-        <li>${food.strIngredient3}</li>
-        <li>${food.strIngredient4}</li>
-        <li>${food.strIngredient5}</li>
-        <li>${food.strIngredient6}</li>
-        <li>${food.strIngredient7}</li>
-        <li>${food.strIngredient8}</li>
-        <li>${food.strIngredient9}</li>
-        <li>${food.strIngredient10}</li>
-        `
-        ul.innerHTML = foodIngredients;
-        foodItems.appendChild(ul);
+    const foodIngredients = `
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${food.strMealThumb}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${food.strMeal}</h5>
+      <p class="card-text">Ingredients</p>
+    </div>
+    <ul class="list-group list-group-flush d-flex align-items-center">
+      <li class="list-group-item">${food.strIngredient1}</li>
+      <li class="list-group-item">${food.strIngredient2}</li>
+      <li class="list-group-item">${food.strIngredient3}</li>
+      <li class="list-group-item">${food.strIngredient4}</li>
+      <li class="list-group-item">${food.strIngredient5}</li>
+      <li class="list-group-item">${food.strIngredient6}</li>
+      <li class="list-group-item">${food.strIngredient7}</li>
+      <li class="list-group-item">${food.strIngredient8}</li>
+      <li class="list-group-item">${food.strIngredient9}</li>
+      <li class="list-group-item">${food.strIngredient10}</li>
+    </ul>
+    </div>    
+   `
+    ul.innerHTML = foodIngredients;
+    foodItems.appendChild(ul);
 }
 
